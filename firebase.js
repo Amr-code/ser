0a75901f0c60fb98cf6b73531270dd4d64fcba2f
amr zhwh <zhwhamr11@gmail.com>
2020-12-07 13:56:17 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var app_firebase = {};
+(function(){
+    var config = {
+        apiKey: "AIzaSyB5NqT3HoCVqO4ibSv774q39OBTsmI3mTM",
+        authDomain: "ser3-bdc72.firebaseapp.com",
+        databaseURL: "https://ser3-bdc72.firebaseio.com",
+        projectId: "ser3-bdc72",
+        storageBucket: "ser3-bdc72.appspot.com",
+        messagingSenderId: "884990256370",
+      };
+      // Initialize Firebase
+      firebase.initializeApp(config);
+      app_firebase = firebase;
+})()
---
+++ ser.js
@@ -0,0 +1,137 @@
+  /*a*/
+  function showDiv() {
+      document.getElementById('chata').style.display = "block";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*b*/
+  function showDivb() {
+      document.getElementById('chatb').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*c*/
+  function showDivc() {
+      document.getElementById('chatc').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*e*/
+  function showDive() {
+      document.getElementById('chate').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*j*/
+  function showDivj() {
+      document.getElementById('chatj').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*d*/
+  function showDivd() {
+      document.getElementById('chatd').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*f*/
+  function showDivf() {
+      document.getElementById('chatf').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*g*/
+  function showDivg() {
+      document.getElementById('chatg').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*h*/
+  function showDivh() {
+      document.getElementById('chath').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chati').style.display = "none";
+  }
+  /*i*/
+  function showDivi() {
+      document.getElementById('chati').style.display = "block";
+      document.getElementById('chata').style.display = "none";
+      document.getElementById('chatb').style.display = "none";
+      document.getElementById('chatc').style.display = "none";
+      document.getElementById('chate').style.display = "none";
+      document.getElementById('chatj').style.display = "none";
+      document.getElementById('chatd').style.display = "none";
+      document.getElementById('chatf').style.display = "none";
+      document.getElementById('chatg').style.display = "none";
+      document.getElementById('chath').style.display = "none";
+
+  }
+
+  document.getElementsByClassName("cover-wrap")[0].addEventListener("click", function() {
+      if (this.classList.contains("active")) {
+          this.classList.remove("active");
+      } else this.classList.add("active");
+  });
